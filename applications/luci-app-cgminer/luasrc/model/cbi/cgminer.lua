44bba339988dd048b591bd0d7ce4e7a03945aad6
Libor Vašíček <libor.vasicek@braiins.cz>
2018-01-05 16:54:51 +0100

    Add theoretical hashrate to cgminer configuration
    - Create constants for miner default values

--- applications/luci-app-cgminer/luasrc/model/cbi/cgminer.lua
+++ applications/luci-app-cgminer/luasrc/model/cbi/cgminer.lua
@@ -18,6 +18,9 @@ local fs = require "nixio.fs"
 local sys = require "luci.sys"
 require "ubus"
 
+local DEFAULT_FREQUENCY = 1332
+local DEFAULT_VOLTAGE = 10
+
 m = Map("cgminer", translate("CGMiner"), translate("Miner general configuration"))
 m.on_after_commit = function() luci.sys.call("/etc/init.d/cgminer reload") end
 
@@ -47,20 +50,31 @@ s.addremove = false
 o = s:option(Value, "frequency", translate("Frequency (MHz)"),
 	translate("If you want to try overclock frequency, you usually need to adjust VID to be lower."))
 o.datatype = "range(120,1332)"
-o.placeholder = "1332"
-o.default = 1332
+o.placeholder = DEFAULT_FREQUENCY
+o.default = DEFAULT_FREQUENCY
 
 o = s:option(Value, "voltage", translate("Voltage (Level)"),
 	translate("The lower VID value means the higher voltage and higher power consumption."))
 o.datatype = "range(10,15)"
-o.placeholder = "13"
-o.default = 13
+o.placeholder = DEFAULT_VOLTAGE
+o.default = DEFAULT_VOLTAGE
 
 o = s:option(StaticList, "chains", translate("Enabled Chains"))
 o.widget = "checkbox"
 o.rmempty = true
 for i=0,5 do
 	o:value(i)
+end
+
+o = s:option(DummyValue, "hashrate", translate("Theoretical Hashrate"))
+function o.cfgvalue(self,section)
+  local CHIPS_PER_CHAIN = 63
+  local CORES_PER_CHIP = 32
+  local frequency = m:get(section, "frequency") or DEFAULT_FREQUENCY
+  local chains = m:get(section, "chains")
+  -- get number of enabled chains
+  chains = (chains and table.getn(chains)) or 0
+  return (frequency * 10^6 * 2 * CHIPS_PER_CHAIN * CORES_PER_CHIP * chains) / 10^12 .. " Th/s"
 end
 
 return m
