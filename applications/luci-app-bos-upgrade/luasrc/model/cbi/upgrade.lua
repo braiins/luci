91666c8ac17958e99d74faa8be90fb01a55ce374
Hynek Fabian <hynek.fabian@braiins.cz>
2020-06-15 16:24:55 +0200

    Fix empty form if auto_upgrade section is missing in uci

--- applications/luci-app-bos-upgrade/luasrc/model/cbi/upgrade.lua
+++ applications/luci-app-bos-upgrade/luasrc/model/cbi/upgrade.lua
@@ -17,6 +17,14 @@ Author: Braiins <braiins@braiins.com>
 local m = Map("bos", translate("System Upgrade"),
 translate("Configure the firmware to monitor our feeds server for new releases and automatically install them."))
 
+-- if config exists but section auto_upgrade does is not present, luci will show an empty form
+-- therefore we create it uncommited, so that cbi can see it and make us a form
+-- note that other luci forms seem to have own uci cursors and should not interfere
+local sect = m.uci:get("bos", "auto_upgrade")
+if sect == nil then
+    m.uci:set("bos", "auto_upgrade", "bos")
+end
+
 local s = m:section(NamedSection, "auto_upgrade", "bos")
 s.anonymous = true
 
